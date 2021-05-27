f7895538852f8de2406c05aa2ee8ae23898551e0
theMagnificentJay <reedjoshua408@gmail.com>
2021-05-27 11:23:27 -0500

    updated and added to NoteController and NoteService

--- ElevenNote.WebAPI/Controllers/NoteController.cs
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -19,6 +19,12 @@ namespace ElevenNote.WebAPI.Controllers
             var notes = noteService.GetNotes();
             return Ok(notes);
         }
+        public IHttpActionResult Get(int id)
+        {
+            NoteService noteService = CreateNoteService();
+            var note = noteService.GetNoteById(id);
+            return Ok(note);
+        }
         public IHttpActionResult Post(NoteCreate note)
         {
             if (!ModelState.IsValid)
